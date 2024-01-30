8023a4db9fd9121d71802909dd3dd8f6e4badea6
Zero <zero.js.dev@gmail.com>
2024-01-30 17:23:39 +0330

    Trying to fix pan and zoom in monitor part #1

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import './reset.scss'
 import './app.scss'
 import { Store } from './store'
-import { Slider } from 'iot-widgets'
+import { Container, Slider } from 'iot-widgets'
 import { startTest } from './test'
 
 const app = document.querySelector<HTMLDivElement>("#app")!
-const store = new Store(app, [new Slider()])
+const containerMain = document.createElement('main')
+const container = new Container({x: 200, y: 200}, 1.4)
+container.appendChild(containerMain)
+const store = new Store(app, container, [new Slider()])
 
 startTest(store, 500)
--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,6 @@
-import { Slider, Gauge, EditBox } from "iot-widgets"
+import { Slider, Gauge, EditBox, Container } from "iot-widgets"
 import { KeyShortcatListener, PanListener, ZoomListener } from "iot-widgets/event"
-import { Point, Transform, randomId } from "iot-widgets/math"
-import { logInfo } from "iot-widgets/utility"
-
-const mouseCoordInZoomAndPan = (e: MouseEvent, pan: Point, zoom: number) => {
-    const dx = e.clientX - e.offsetX
-    const dy = e.clientY - e.offsetY
-
-    return {
-        x: e.clientX - dx,
-        y: e.clientY - dy,
-    }
-}
+import { Point, randomId } from "iot-widgets/math"
 
 const mouseCoordInZoomAndPan2 = (e: MouseEvent, pan: Point, zoom: number) => {
     return {
@@ -20,25 +9,21 @@ const mouseCoordInZoomAndPan2 = (e: MouseEvent, pan: Point, zoom: number) => {
     }
 }
 
-const mouseCoordShow = (e: MouseEvent) => {
-    return { x: e.clientX, y: e.clientY}
-}
-
 type IWidgets = Slider & Gauge
 
 export class Store {
     app: HTMLDivElement
-    widgetEditBox: { id: string, editBox: EditBox }[] 
+    widgetEditBox: { id: string, editBox: EditBox }[]
     panListener: PanListener<{ clientX: number, clientY: number }>
     zoomListener: ZoomListener
     digit0keyListener: KeyShortcatListener
-    containerTransform: Transform
-    constructor(app: HTMLDivElement, widgets: IWidgets[] = [], pan = { x: 0, y: 0 }, zoom = 1) {
+    container: Container
+    constructor(app: HTMLDivElement, container: Container, widgets: IWidgets[] = []) {
         this.app = app
         this.widgetEditBox = []
-        widgets.forEach(widget => this.app.appendChild(widget)) 
-        this.setContainerTransform = {x: pan.x, y: pan.y, scaleX: zoom}       
-        this.containerTransform = new Transform(this.app) 
+        this.container = container
+        this.app.appendChild(this.container)
+        widgets.forEach(widget => this.container.firstChild!.appendChild(widget))
 
         // Event
         this.digit0keyListener = new KeyShortcatListener('Digit0')
@@ -46,61 +31,60 @@ export class Store {
         this.zoomListener = new ZoomListener()
         this.initHandler()
 
-        document.addEventListener('mousemove', (e) => {
-            const currentMouseCoord = mouseCoordShow(e)
-            
-            logInfo({
-                points: [
-                    {
-                        label: 'm',
-                        point: {
-                            x: currentMouseCoord.x + 10,
-                            y: currentMouseCoord.y + 10,
-                        }
-                    }
-                ]
-            })
-        })
-    }
-    public set setContainerTransform(transform: { x: number, y: number, scaleX: number }) {
-        this.app.style.transform = `translate(${transform.x}px,${transform.y}px) scale(${transform.scaleX})`
+        // document.addEventListener('mousemove', (e) => {
+        //     const currentMouseCoord = mouseCoordShow(e)
+
+        //     logInfo({
+        //         points: [
+        //             {
+        //                 label: 'm',
+        //                 point: {
+        //                     x: currentMouseCoord.x + 10,
+        //                     y: currentMouseCoord.y + 10,
+        //                 }
+        //             }
+        //         ]
+        //     })
+        // })
     }
 
     initHandler() {
-        document.addEventListener('click' , this.handleClick)
+        document.addEventListener('click', this.handleClick)
         this.digit0keyListener.onKeyDown = (e) => {
             if (e.ctrlKey) {
-                this.setContainerTransform = {x: 0, y: 0, scaleX: 1}
+                this.container.zoom = 1
+                this.container.pan = { x: 0, y: 0 }
             }
         }
         this.panListener.onPanStart = (e, initFn) => {
-            const transform = this.containerTransform.transform
-            initFn({ clientX: e.clientX - transform.x, clientY: e.clientY - transform.y })
+            const pan = this.container.pan
+            initFn({ clientX: e.clientX - pan.x, clientY: e.clientY - pan.y })
         }
         this.panListener.onPanMove = (e, init) => {
-            const transform = this.containerTransform.transform
-            if(init) {
+            const pan = this.container.pan
+            if (init) {
                 const x = e.clientX - init.clientX
                 const y = e.clientY - init.clientY
 
-                this.setContainerTransform = {...transform, x, y}
+                this.container.pan = { ...pan, x, y }
             }
         }
         this.zoomListener.onZoom = (e) => {
             if (e.ctrlKey) {
                 e.preventDefault()
-                const {scaleX, ...pan} = this.containerTransform.transform
-                const currentMouseCoord = mouseCoordInZoomAndPan2(e, pan, scaleX)
+                const pan = this.container.pan
+                const zoom = this.container.zoom
+                const currentMouseCoord = mouseCoordInZoomAndPan2(e, pan, zoom)
                 // const currentMouseCoord = {x: e.clientX, y: e.clientY}
 
-                const xs = (currentMouseCoord.x - pan.x) / scaleX
-                const ys = (currentMouseCoord.y - pan.y) / scaleX
-                let zoom = scaleX
+                const xs = (currentMouseCoord.x - pan.x) / zoom
+                const ys = (currentMouseCoord.y - pan.y) / zoom
+                this.container.zoom = zoom
                 if (((e as any)?.wheelDelta ? (e as any).wheelDelta : -e.deltaY) > 0)
-                    zoom *= 1.2
+                    this.container.zoom = zoom * 1.2
                 else
-                    zoom /= 1.2
-                this.setContainerTransform = { x: currentMouseCoord.x - xs * zoom, y: currentMouseCoord.y - ys * zoom, scaleX: zoom }
+                    this.container.zoom = zoom / 1.2
+                this.container.pan = { x: currentMouseCoord.x - xs * zoom, y: currentMouseCoord.y - ys * zoom }
             }
         }
     }
@@ -108,7 +92,7 @@ export class Store {
         this.attachEditBox(e.target as any)
     }
     push(widget: IWidgets) {
-        this.app.appendChild(widget)
+        this.container.firstChild!.appendChild(widget)
     }
     attachEditBox(widget: HTMLElement) {
         const widgetId = widget.getAttribute('id')
@@ -124,7 +108,7 @@ export class Store {
                 const rotate = +widget.getAttribute('rotate')!
                 const origin = widget.getAttribute('origin')!
 
-                const editBox = new EditBox(randomId(), this.containerTransform  as any, width, height, x, y, rotate, origin)
+                const editBox = new EditBox(randomId(), this.container as any, width, height, x, y, rotate, origin)
                 this.widgetEditBox.push({ id: widgetId, editBox })
                 editBox.onEdit = (e) => {
                     widget.setAttribute('width', e.width.toString())
@@ -134,12 +118,12 @@ export class Store {
                     widget.setAttribute('x', e.x.toString())
                     widget.setAttribute('y', e.y.toString())
                 }
-                this.app.appendChild(editBox)
+                this.container.firstChild!.appendChild(editBox)
             }
-        }        
+        }
 
         // Remove EditBoxs
-        if(widgetId === 'app' || !widgetId) {
+        if (widgetId === 'app' || !widgetId) {
             this.widgetEditBox.forEach(item => item.editBox.remove())
             this.widgetEditBox = []
         }
